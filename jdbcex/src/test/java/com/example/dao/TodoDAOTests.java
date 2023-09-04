940ab74c102a5902b4a2ebf1567a1fd48a4d1617
GunukLyoo <gunuk418@gmail.com>
2023-09-04 22:53:33 +0900

    studying

--- jdbcex/src/test/java/com/example/dao/TodoDAOTests.java
+++ jdbcex/src/test/java/com/example/dao/TodoDAOTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class TodoDAOTests {
 
@@ -29,5 +30,32 @@ public class TodoDAOTests {
                 .build();
 
         todoDAO.insert(vo);
+    }
+
+    @Test
+    public void testList() throws Exception{
+        List<TodoVO> list = todoDAO.selectAll();
+
+        list.forEach(vo -> System.out.println(vo));
+    }
+
+    @Test
+    public  void testSelectOne() throws Exception{
+        Long tno = 1L;
+        TodoVO vo = todoDAO.selectOne(tno);
+
+        System.out.println(vo);
+    }
+
+    @Test
+    public void testUpdateOne() throws Exception{
+        TodoVO vo = TodoVO.builder()
+                .tno(1L)
+                .title("Sample Title...")
+                .dueDate(LocalDate.of(2021,12,31))
+                .finished(true)
+                .build();
+
+        todoDAO.updateOne(vo);
     }
 }
